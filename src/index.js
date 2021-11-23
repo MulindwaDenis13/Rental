ce8ad1c970033aa0427f7307322c8fe0314fc469
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-11-23 17:35:43 +0300

    package-installations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "@fontsource/ubuntu";
+import App from "./App";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
-ReactDOM.render(
-  <React.StrictMode>
+const THEME = createMuiTheme({
+  typography: {
+    fontFamily: `"Ubuntu", sans-serifs`,
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+  },
+});
+
+const AppWithTheme = () => (
+  <MuiThemeProvider theme={THEME}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </MuiThemeProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<AppWithTheme />, document.getElementById("root"));
