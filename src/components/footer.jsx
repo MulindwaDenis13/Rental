786c7bb81af61cb4bdf822a588813e61168dbdc5
MulindwaDenis13 <mulindwadenis13@gmail.com>
2021-11-24 11:06:28 +0300

    adding styles and shared components

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  return (
+    <>
+      <footer>
+        <h5>
+          <label htmlFor="nav-toggle">
+            <span className="las la-bars"></span>
+          </label>
+          <span className="health_unit_name">
+            Group One Copyright Reserved &copy; 2021
+          </span>
+        </h5>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
